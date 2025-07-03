b743341d02fdcd929b51a3ba8fea68b50fc04d87
Raveesh-Gautam <raveeshgautamfzd2002@gmail.com>
2025-07-03 11:30:26 +0530

    render expense form conditionaly

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,10 +1,10 @@
 // Write your code at relevant places in the code below:
 
-import { useState } from "react";
-import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
-import "./Expenses.css";
+import React, { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
+import Card from "../UI/Card";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2023");
@@ -17,46 +17,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found</p>;
-
- if (filteredExpenses.length === 1) {
-  expensesContent = (
-    <>
-      <ExpenseItem
-        key={filteredExpenses[0].id}
-        title={filteredExpenses[0].title}
-        date={filteredExpenses[0].date}
-        price={filteredExpenses[0].price}
-      />
-      <p className="only-one-expense-message">
-        Only one expense found. Please add more
-      </p>
-    </>
-  );
-}
-
-
-
-  if (filteredExpenses.length > 1) {
-    expensesContent = filteredExpenses.map((expense) => {
-      return (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          date={expense.date}
-          price={expense.price}
-        />
-      );
-    });
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={changeFilterHandler}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 };
--- src/components/NewExpenses/NewExpense.js
+++ src/components/NewExpenses/NewExpense.js
@@ -1,17 +1,43 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+const [isEditing,setIsEditing]=useState(false);
+
    const saveExpenseDataHandler=(enterdData)=>{
     console.log(enterdData);
     const expenseData={...enterdData,id:Math.random().toString()};
     console.log(expenseData);
      props.onAddExpense(expenseData);
+     setIsEditing(false);
+ }
+ const startEditingHandler=()=>{
+      setIsEditing(true);
+ }
+  const stopEditingHandler=()=>{
+      setIsEditing(false);
+ }
+ const CancleHandler=()=>{
+    setIsEditing(false);
+ }
 
-    }
   return (
     <div className="new-expense">
-       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+        {!isEditing&& (
+        <button onClick={startEditingHandler}>Add Expense</button>
+      )}
+      {
+        isEditing &&
+
+        (
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} 
+        onCancel={stopEditingHandler}/>
+       
+      
+    )
+      }
+       
     </div>
   );
 };
